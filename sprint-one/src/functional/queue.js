3442a2579e549829b9ed183030162c9d582ed444
Tenzin Sonam <tenso2006@gmail.com>
2016-12-27 13:46:26 -0800

    Done: 'queue' in The functional pattern

--- sprint-one/src/functional/queue.js
+++ sprint-one/src/functional/queue.js
@@ -5,14 +5,25 @@ var Queue = function() {
   var storage = {};
 
   // Implement the methods below
+  var first = 0;
+  var last = 0;
 
   someInstance.enqueue = function(value) {
+    storage[last] = value;
+    last++;
   };
 
   someInstance.dequeue = function() {
+    var temp = storage[first];
+    delete storage[first++];
+    return temp;
   };
 
   someInstance.size = function() {
+    if(last - first < 0){
+      return 0;
+    }
+    return last - first;
   };
 
   return someInstance;
