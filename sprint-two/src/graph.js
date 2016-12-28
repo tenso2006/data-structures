3b54c18119cfb583a97e4884bd133d64995dfe9f
Tenzin Sonam <tenso2006@gmail.com>
2016-12-27 23:07:03 -0800

    Done: graph -data structure

--- sprint-two/src/graph.js
+++ sprint-two/src/graph.js
@@ -2,38 +2,57 @@
 
 // Instantiate a new graph
 var Graph = function() {
+  this.nodes = {};
 };
 
 // Add a node to the graph, passing in the node's value.
 Graph.prototype.addNode = function(node) {
+  this.nodes[node] = node;
 };
 
 // Return a boolean value indicating if the value passed to contains is represented in the graph.
 Graph.prototype.contains = function(node) {
+  return this.nodes.hasOwnProperty(node) ;
 };
 
 // Removes a node from the graph.
 Graph.prototype.removeNode = function(node) {
+  delete this.nodes[node];
 };
 
 // Returns a boolean indicating whether two specified nodes are connected.  Pass in the values contained in each of the two nodes.
 Graph.prototype.hasEdge = function(fromNode, toNode) {
+  return this.nodes[fromNode] === toNode || this.nodes[toNode] === fromNode;
 };
 
 // Connects two nodes in a graph by adding an edge between them.
 Graph.prototype.addEdge = function(fromNode, toNode) {
+  this.nodes[fromNode] = toNode;
 };
 
 // Remove an edge between any two specified (by value) nodes.
 Graph.prototype.removeEdge = function(fromNode, toNode) {
+  if(this.nodes[fromNode] === toNode || this.nodes[toNode] === fromNode) {
+    delete this.nodes[fromNode];
+  }
+
 };
 
 // Pass in a callback which will be executed on each node of the graph.
 Graph.prototype.forEachNode = function(cb) {
+  _.each(this.nodes, function(node, key) {
+    cb(node, key);
+  });
 };
 
 /*
  * Complexity: What is the time complexity of the above functions?
+
+    addNode: Constant time
+    contains: Constant time
+    removeNode: Linear time
+    hasEdge: Constant time
+    addEdge: Constant time
+    removeEdge: Constant time
+    forEachNode: Linear time
  */
-
-
