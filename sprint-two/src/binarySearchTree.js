7ca1d569eb82c412058faedc75097cb12f202ed7
Tenzin Sonam <tenso2006@gmail.com>
2016-12-28 13:05:54 -0800

    done: binarySearchTree - contains and depthFirstLog method

--- sprint-two/src/binarySearchTree.js
+++ sprint-two/src/binarySearchTree.js
@@ -27,12 +27,37 @@ binaryTreeMethods.insert = function (value) {
 };
 
 binaryTreeMethods.contains = function (value) {
-
+  if (this.value === value) {
+    return true;
+  }
+  if (value < this.value) {
+    if (this.left !== null) {
+      return this.left.contains(value);
+    } else {
+      return false;
+    }
+  } else if (value > this.value) {
+    if (this.right !== null) {
+      return this.right.contains(value);
+    } else {
+      return false;
+    }
+  }
 };
 
 binaryTreeMethods.depthFirstLog = function (callback) {
-
+  callback(this.value);
+  if (this.left) {
+    this.left.depthFirstLog(callback);
+  }
+  if (this.right) {
+    this.right.depthFirstLog(callback);
+  }
 };
 /*
  * Complexity: What is the time complexity of the above functions?
+
+   insert:          Log N
+   contains:        Log N
+   depthFirstLog:   Linear
  */
