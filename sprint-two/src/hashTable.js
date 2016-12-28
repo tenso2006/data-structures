c5bc34c573ef38f4d755780812d2e70eaab45118
Tenzin Sonam <tenso2006@gmail.com>
2016-12-28 09:08:44 -0800

    done: Hash function - insert and retrieve

--- sprint-two/src/hashTable.js
+++ sprint-two/src/hashTable.js
@@ -3,14 +3,35 @@
 var HashTable = function() {
   this._limit = 8;
   this._storage = LimitedArray(this._limit);
+
 };
 
 HashTable.prototype.insert = function(k, v) {
   var index = getIndexBelowMaxForKey(k, this._limit);
+
+  var bucket = this._storage.get(index) || [];
+
+  _.each(bucket, function(tuple, i){
+    if (tuple[0] === k) {
+      //var temp = tuple[1];
+      tuple[1] = v;
+    }
+  });
+  bucket.push([k,v]);
+  this._storage.set(index, bucket);
+
 };
 
 HashTable.prototype.retrieve = function(k) {
   var index = getIndexBelowMaxForKey(k, this._limit);
+
+  var bucket = this._storage.get(index);
+  for (var i = 0; i < bucket.length; i++) {
+    var tuple = bucket[i];
+    if(tuple[0] === k) {
+      return tuple[1];
+    }
+  }
 };
 
 HashTable.prototype.remove = function(k) {
