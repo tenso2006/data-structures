ecf574dc72bd6831b4e307dee36a5f2877bc7216
Tenzin Sonam <tenso2006@gmail.com>
2016-12-28 09:23:54 -0800

    done: hashTable - remove

--- sprint-two/src/hashTable.js
+++ sprint-two/src/hashTable.js
@@ -11,13 +11,13 @@ HashTable.prototype.insert = function(k, v) {
 
   var bucket = this._storage.get(index) || [];
 
-  _.each(bucket, function(tuple, i){
+  _.each (bucket, function(tuple, i) {
     if (tuple[0] === k) {
       //var temp = tuple[1];
       tuple[1] = v;
     }
   });
-  bucket.push([k,v]);
+  bucket.push([k, v]);
   this._storage.set(index, bucket);
 
 };
@@ -36,6 +36,16 @@ HashTable.prototype.retrieve = function(k) {
 
 HashTable.prototype.remove = function(k) {
   var index = getIndexBelowMaxForKey(k, this._limit);
+
+  var bucket = this._storage.get(index);
+
+  for (var i = 0; i < bucket.length; i++) {
+    var tuple = bucket[i];
+    if (tuple[0] === k) {
+      bucket.splice(i,1);
+      return;
+    }
+  }
 };
 
 
