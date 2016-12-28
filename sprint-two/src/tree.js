b4383b016990471218366156a735d12370732947
Tenzin Sonam <tenso2006@gmail.com>
2016-12-27 20:54:01 -0800

    Done: Tree - data structure

--- sprint-two/src/tree.js
+++ sprint-two/src/tree.js
@@ -2,8 +2,9 @@ var Tree = function(value) {
   var newTree = {};
   newTree.value = value;
 
-  // your code here
-  newTree.children = null;  // fix me
+  newTree.children = [];
+
+  _.extend(newTree, treeMethods);
 
   return newTree;
 };
@@ -11,13 +12,35 @@ var Tree = function(value) {
 var treeMethods = {};
 
 treeMethods.addChild = function(value) {
+  var childTree = Tree(value);
+  this.children.push(childTree);
 };
 
 treeMethods.contains = function(target) {
+
+  var contain = false;
+
+  var checkTarget = function (node, target) {
+    if(node.value === target) {
+      contain = true;
+    } else if (node.children.length) {
+      for (var i = 0; i < node.children.length; i++) {
+        checkTarget(node.children[i], target);
+      }
+    }
+  };
+
+
+  checkTarget(this, target);
+  return contain;
 };
 
 
 
 /*
  * Complexity: What is the time complexity of the above functions?
+
+    addChild : Linear
+    contains:  nlog(n)
+
  */
