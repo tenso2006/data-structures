df038ba3fd058119853def587344caa3cc2263df
Tenzin Sonam <tenso2006@gmail.com>
2016-12-27 23:29:43 -0800

    Done: set -data structures

--- sprint-two/src/set.js
+++ sprint-two/src/set.js
@@ -1,20 +1,28 @@
 var Set = function() {
   var set = Object.create(setPrototype);
-  set._storage = null; // fix me
+  set._storage = {};
   return set;
 };
 
 var setPrototype = {};
 
 setPrototype.add = function(item) {
+  this._storage[item] = true;
 };
 
 setPrototype.contains = function(item) {
+  return !!this._storage[item];
 };
 
 setPrototype.remove = function(item) {
+  delete this._storage[item];
 };
 
 /*
- * Complexity: What is the time complexity of the above functions?
+ * Time Complexity:
+
+  add: constant time
+  contains: constant time
+  remove: constant time
+
  */
